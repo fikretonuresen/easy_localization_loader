f3ff671f12356b15a38a4cad8dfc5cbb330c7a73
Fikret Onur Esen <fikretonuresen@gmail.com>
2024-12-04 14:17:52 +0300

    Enhance FileAssetLoader to support locale-specific paths and add FileSingleAssetLoader

--- lib/src/file_asset_loader.dart
+++ lib/src/file_asset_loader.dart
@@ -8,6 +8,23 @@ import 'package:easy_localization/easy_localization.dart';
 class FileAssetLoader extends AssetLoader {
   const FileAssetLoader();
 
+  String getLocalePath(String basePath, Locale locale) {
+    return '$basePath/${locale.toStringWithSeparator(separator: "-")}.json';
+  }
+
+  @override
+  Future<Map<String, dynamic>> load(String path, Locale locale) async {
+    var localePath = getLocalePath(path, locale);
+    final file = File(localePath);
+    log('easy localization loader: load file $localePath');
+
+    return json.decode(await file.readAsString());
+  }
+}
+
+class FileSingleAssetLoader extends AssetLoader {
+  const FileSingleAssetLoader();
+
   @override
   Future<Map<String, dynamic>> load(String path, Locale locale) async {
     final file = File(path);
